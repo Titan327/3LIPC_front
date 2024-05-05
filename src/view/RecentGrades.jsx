7e0337bba2f159a1cb76e0a471941b873507f048
Tristan Tourbier <tristan.tourbier@outlook.fr>
2024-05-05 14:57:53 +0200

    s

--- src/view/RecentGrades.jsx
+++ src/view/RecentGrades.jsx
@@ -3,11 +3,14 @@ import Courses from "../request/courses";
 
 function RecentGrades() {
 
+    const [data, setData] = useState(null);
+
     useEffect(() => {
         const fetchData = async () => {
             try {
                 const responseData = await Courses.GetMyNotes();
                 console.log(responseData);
+                setData(responseData);
             } catch (error) {
                 console.error('Error fetching data:', error);
             }
@@ -17,6 +20,7 @@ function RecentGrades() {
     }, []);
 
     //TODO: faire la requete a l'api pour recup le tableau
+    /*
     const tabData = [
         ["Subject","Teacher","Exercise","Grade"],
         ["Python","Ryan Dordain","Boucle while","15/20"],
@@ -26,11 +30,57 @@ function RecentGrades() {
         ["Python","Ryan Dordain","Boucle while","15/20"],
         ["Python","Ryan Dordain","Boucle while","15/20"]
     ]
+     */
 
-    const tabHead = tabData[0].map((element, index) => (
-        <th scope="col" key={index}>{element}</th>
-    ));
 
+
+    let content;
+    if (data === null) {
+        content = <div>Loading...</div>;
+    } else {
+
+        const dataHead = ["Subject", "Note"]
+
+        const tabHead = dataHead.map((element, index) => (
+            <th scope="col" key={index}>{element}</th>
+        ));
+
+        const cleanData = data.notes.map(objet => {
+            return {
+                Subject: objet.Subject.label,
+                Note: objet.note
+            };
+        });
+
+        const tabData = cleanData.map((row, index) => (
+            <tr key={index}>
+                <td>{row.Subject}</td>
+                <td>{row.Note}</td>
+            </tr>
+        ));
+
+        content = (
+            <table className="table">
+                <thead>
+                <tr>
+                    {tabHead}
+                </tr>
+                </thead>
+                <tbody>
+                    {tabData}
+                </tbody>
+            </table>
+        );
+
+
+
+    }
+
+
+
+
+
+/*
     const tabBody = tabData.slice(1).map((element, index) => (
 
         <tr key={index}>
@@ -42,19 +92,11 @@ function RecentGrades() {
         </tr>
     ));
 
+ */
+
     return (
         <div className="d-flex align-items-center" style={{width:"90vw"}}>
-            <table className="table">
-                <thead>
-                    <tr>
-
-                        {tabHead}
-                    </tr>
-                </thead>
-                <tbody>
-                    {tabBody}
-                </tbody>
-            </table>
+            {content}
         </div>
     );
 }
